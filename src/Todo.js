1618de31dd4e8fdd76801be7196320a3fa59bca5
JM <limjungmok@gmail.com>
2018-06-23 17:59:30 +0900

    localStorage 완성

--- src/Todo.js
+++ src/Todo.js
@@ -3,21 +3,30 @@
 */
 'use strict';
 function Todo() {
-  var storage = {}
+  var myStorage = localStorage;
+  var storage = {};
 
   return {
     getStorage: function() {
+      if(myStorage) storage = JSON.parse(myStorage.data);
+      
       return storage;
     },
     createTodo: function(hash, obj) {
       if(!storage[hash]) storage[hash] = [];
       storage[hash].push(obj);
+      
+      if(myStorage) myStorage.data = JSON.stringify(storage);
     },
-    removeTodo: function(key, index) {
+    removeTodo: function(key, index) {      
       storage[key].splice(index, 1);
+      
+      if(myStorage) myStorage.data = JSON.stringify(storage)
     },
     editTodo: function(key, index, title) {
       storage[key][index].title = title;
+      
+      if(myStorage) myStorage.data = JSON.stringify(storage);
     }
   };
 }
